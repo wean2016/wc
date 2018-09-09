70162461f9f4df859936c7f2f407ac445060f709
wean <weanyq@gmail.com>
2018-09-09 21:03:13 +0800

    完成统计字符数(-c)功能

---
+++ src/main/java/Count.java
@@ -0,0 +1,15 @@
+/**
+ * @version V1.0.0
+ * @Description
+ * @Author liuyuequn weanyq@gmail.com
+ * @Date 2018/9/9 20:41
+ */
+public class Count {
+    /**
+     * 打印出输入文本的字符数
+     * @param text
+     */
+    public static void count(String text){
+        System.out.println(String.format("字符数为: %d", text.length()));
+    }
+}
---
+++ src/main/java/WC.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * @version V1.0.0
+ * @Description
+ * @Author liuyuequn weanyq@gmail.com
+ * @Date 2018/9/9 20:21
+ */
+public class WC {
+    public static void main(String[] args) {
+        if (args.length <= 0){
+            System.out.println("请输入参数");
+            return;
+        }
+        // 拿到文件
+        String fileName = args[args.length - 1];
+        String text = ReadToString.readToString(fileName);
+        if (text == null){
+            System.out.println("文件不存在!");
+            return;
+        }
+
+        // 并行处理各个参数(忽略最后一个参数， 即忽略文件名)
+        Arrays.asList(args).subList(0, args.length-1).parallelStream().forEach(s -> {
+            switch (s){
+                case "-c" : Count.count(text); break;
+                default: break;
+            }
+        });
+
+    }
+
+
+}
